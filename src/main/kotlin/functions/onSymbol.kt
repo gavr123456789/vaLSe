d63c4dec8600723ba12dd857c2e15cd909ba4fbf
gavr123456789 <gavr123456789@gmail.com>
2024-07-21 03:42:28 +0300

    [hover] internal types doesn't contain definitions

--- src/main/kotlin/functions/onSymbol.kt
+++ src/main/kotlin/functions/onSymbol.kt
@@ -14,20 +14,20 @@ import java.io.File
 import java.net.URI
 
 
-fun MessageMetadata.toDocumentSymbol(): DocumentSymbol {
-    val range = this.declaration!!.token.toLspPosition()
+fun MessageMetadata.toDocumentSymbol(): DocumentSymbol? {
+    val range = this.declaration?.token?.toLspPosition() ?: return null//throw Exception("MessageMetadata.declaration is null, MessageMetadata is $this ")
     return DocumentSymbol(this.toString(), SymbolKind.Method, range, range)
 }
 
-fun createHierarchyFromType(type: Type, token: Token): DocumentSymbol {
+fun createHierarchyFromType(type: Type, token: Token, client: LanguageClient): DocumentSymbol {
 
     val sas = token.toLspPosition()
-
+    client.info("createHierarchyFromType from $type, from token: $token")
     val methods = type.protocols.flatMap { protocol ->
-        protocol.value.unaryMsgs.map { it.value.toDocumentSymbol() } +
-        protocol.value.binaryMsgs.map { it.value.toDocumentSymbol() } +
-        protocol.value.keywordMsgs.map { it.value.toDocumentSymbol() } +
-        protocol.value.builders.map { it.value.toDocumentSymbol() }
+        protocol.value.unaryMsgs.mapNotNull { it.value.toDocumentSymbol() } +
+        protocol.value.binaryMsgs.mapNotNull { it.value.toDocumentSymbol() } +
+        protocol.value.keywordMsgs.mapNotNull { it.value.toDocumentSymbol() } +
+        protocol.value.builders.mapNotNull { it.value.toDocumentSymbol() }
     }
     val typeSymbol = DocumentSymbol(type.name, SymbolKind.Class, sas, sas, type.toString(), methods)
 
@@ -41,7 +41,7 @@ fun onSymbol(ls: LS, client: LanguageClient, params: DocumentSymbolParams): List
 
     ls.fileToDecl[uriFile]?.let {
         return it.filterIsInstance<SomeTypeDeclaration>().map {
-            createHierarchyFromType(it.receiver!!, it.token)
+            createHierarchyFromType(it.receiver!!, it.token, client)
         }
     }
 
